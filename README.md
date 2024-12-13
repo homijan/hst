916ff0347b497ddc55f5ba1a607647cf9de6a639
Milan Holec <homijan@gmail.com>
2024-12-13 10:31:53 -0700

    Python package (#3)
    * toml package file.
    
    Added savez of G_operators.
    
    * Pure CSR format of G_operators.
    
    * Updated README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Heisenberg scattering transform (HST)
 
+## Install
+
+Our `hst` python package can be installed locally following these steps
+
+1. Clone the package source code (now public) `git clone https://github.com/homijan/hst.git`
+
+2. Install the python package locally (editable mode) `pip install -e hst`
+
+3. Check if you can load the following from the module in `python` in terminal
+
+   `>>> from hst.mrw1d import generate_G_operators, save_G_operators, data_decomposition, data_reconstruction`
+
+## Demo
+
+Copy the **demo** script from the repository `hst/tests/test_wavelet_decomposition.py` which shows how to *decompose* (transform) and *reconstruct* (inverse transform) any given 1D data with `db1` or `db2` Daubechies wavelets. Set `verify_Gs = True` to see that `G_operators` correspodning to *mother* (`G` or `G_lo`) and *daughter* (`bar_G` or `G_hi`) wavelet operators are orthogonal and invertible. 
+
+The matrix `input_data` can be adjusted using your 1D dataset. 
+
 ## Multiresolution wavelet methodology
+
+TBD
--- tests/test_wavelet_decomposition.py
+++ tests/test_wavelet_decomposition.py
@@ -1,15 +1,17 @@
 import numpy as np
-from hst.mrw1d import generate_G_operators, data_decomposition, data_reconstruction
+from hst.mrw1d import generate_G_operators, save_G_operators, data_decomposition, data_reconstruction
 
+# Save G_operators
+save_Gs = False#True
 # Print out essential properties of G_operators
-verify_Gs = True
+verify_Gs = False#True
 
-Nlevels = 6
-Ndata = 20
-data_length = 1024 # Should be a power of 2
+Nlevels = 7
+Ndata = 1000
+data_length = 2**Nlevels * 400 # constructed as a proper  power of 2
 
 # Generate data
-mat_data = np.random.rand(data_length, Ndata)
+input_data = np.random.rand(data_length, Ndata)
 
 print(f'Nlevels {Nlevels}, data_length {data_length}, Ndata {Ndata}')
 
@@ -19,18 +21,28 @@ for wavelet in wavelets:
     print(f'TEST: wavelet {wavelet}')
     # Generate orthogonal G_lo (aka G) and G_hi (aka bar_G) operators
     G_operators = generate_G_operators(wavelet, Nlevels, data_length)
+    print('G_operators generated.')
+    if (save_Gs):
+        # Save generated G_operators using dictionary
+        file_name = f'G_operators_{wavelet}.npz'
+        save_G_operators(G_operators, file_name)
+        print(f'G_operators saved into {file_name}.')
     # Generate data decomposition into (phi_J, bar_phi_J, .., bar_phi_1)
-    decomposition = data_decomposition(G_operators, mat_data, verify_Gs)
+    print('Compute data decomposition:')
+    decomposition = data_decomposition(G_operators, input_data, verify_Gs)
+    print('Decomposition done!')
     # Coarse phi_J interpretation
     cA = decomposition[0]
     print(f'wavelet {wavelet}, cA {cA}')
 
     # Generate data reconstruction from (phi_J, bar_phi_J, .., bar_phi_1) 
-    data = data_reconstruction(decomposition, G_operators)
+    print('Compute data reconstruction:')
+    reconstructed_data = data_reconstruction(decomposition, G_operators)
+    print('Reconstruction done!')
 
     # Verification of direct and inverse multiresolution decomposition and reconstruction
     print(f'Verfication of the multiresolution decomposition and inverse reconstruction:')
-    print(f'mat_data.shape {mat_data.shape}, data.shape {data.shape}')
-    print('mat_data[:, :] - data[:, :]')
-    print(f'{mat_data[:, :] - data[:, :]}')
+    print(f'input_data.shape {input_data.shape}, reconstructed_data.shape {reconstructed_data.shape}')
+    print('input_data[:, :] - reconstructed_data[:, :]')
+    print(f'{input_data[:, :] - reconstructed_data[:, :]}')
     
