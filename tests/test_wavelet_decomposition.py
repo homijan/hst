1055110fc805375df42fe9845c4ff46531abfccf
Milan Holec <homijan@gmail.com>
2024-12-13 13:00:23 -0700

    Added ranodomized SG input data.

--- tests/test_wavelet_decomposition.py
+++ tests/test_wavelet_decomposition.py
@@ -1,5 +1,15 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from hst.mrw1d import generate_G_operators, save_G_operators, data_decomposition, data_reconstruction
+
+# Multi-coefficient super-Gaussian data generation
+def superGaussian(x, cs): 
+    data_length = len(x)
+    Ndata = cs.shape[0]
+    input_data = np.zeros((data_length, Ndata))
+    for i in range(Ndata):
+        input_data[:, i] = cs[i, 0]*np.exp(-((x-cs[i, 1])/cs[i, 2])**(2*cs[i, 3]))
+    return input_data
 
 # Save G_operators
 save_Gs = False#True
@@ -7,11 +17,30 @@ save_Gs = False#True
 verify_Gs = False#True
 
 Nlevels = 7
-Ndata = 5000
-data_length = 2**Nlevels * 100 # constructed as a proper  power of 2
+Ndata = 15 # Number of randomized super-Gaussian (small number to plot)
+data_length = 2**Nlevels * 200 # constructed as a proper  power of 2
+
+#Nlevels = 3
+#Ndata = 10
+#data_length = 1024 # constructed as a proper  power of 2
 
 # Generate data
-input_data = np.random.rand(data_length, Ndata)
+domain_size = 1.0
+x = np.linspace(-domain_size/2.0, domain_size/2.0, data_length)
+# Random super-Gaussian c0*exp(-((x-c1)/c2)^(2*c3))
+input_coeffs = np.random.rand(Ndata, 4)
+# Rescale coeff values, so c0 in (0, 1), c1 in (-domain_size/2, domain_size/2), c2 in (0, domain_size/2), and c3 in {1, .., 10}
+for i in range(Ndata):
+    # c0 amplitude scaling is already correct
+    input_coeffs[i, 0] = input_coeffs[i, 0]
+    # c1 shift scaling
+    input_coeffs[i, 1] = 0.8 * domain_size * (input_coeffs[i, 1] - 0.5)
+    # c2 width scaling
+    input_coeffs[i, 2] = 0.125 * domain_size * input_coeffs[i, 2]
+    # c3 super-Gaussian power scaling
+    input_coeffs[i, 3] = int(10 * input_coeffs[i, 3])
+input_data = superGaussian(x, input_coeffs)
+#input_data = np.random.rand(data_length, Ndata)
 
 print(f'Nlevels {Nlevels}, data_length {data_length}, Ndata {Ndata}')
 
@@ -33,7 +62,8 @@ for wavelet in wavelets:
     print('Decomposition done!')
     # Coarse phi_J interpretation
     cA = decomposition[0]
-    print(f'wavelet {wavelet}, cA {cA}')
+    #print(f'wavelet {wavelet}, cA')
+    #print(f'{cA}')
 
     # Generate data reconstruction from (phi_J, bar_phi_J, .., bar_phi_1) 
     print('Compute data reconstruction:')
@@ -45,4 +75,9 @@ for wavelet in wavelets:
     print(f'input_data.shape {input_data.shape}, reconstructed_data.shape {reconstructed_data.shape}')
     print('input_data[:, :] - reconstructed_data[:, :]')
     print(f'{input_data[:, :] - reconstructed_data[:, :]}')
-    
+
+    # Plot results
+    for i in range(Ndata):
+        plt.plot(x, input_data[:, i])
+        plt.plot(x, reconstructed_data[:, i], '-.')
+    plt.show()    
