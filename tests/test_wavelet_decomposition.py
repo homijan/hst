c87ce6d30f10e0078213208d97462ff35f3cda7e
Milan Holec <homijan@gmail.com>
2024-12-18 23:37:32 -0700

    Solved the orthogonal and invertible BC for complex SDW2.

--- tests/test_wavelet_decomposition.py
+++ tests/test_wavelet_decomposition.py
@@ -92,10 +92,106 @@ wavelets.append(['db2', dec_lo, [(4.0/3.0)**0.5]])
 a1 = 0.662912 + 0.171163j
 a2 = 0.110485 - 0.085581j
 a3 = -0.066291 - 0.085581j
+
+####################################################
+# Orthogonal and invertibel scaling of BC for SDW2 #
+####################################################
+# Find the analytical solution for delta
+from sympy import symbols, Eq, solve, I, simplify, lambdify, Matrix, conjugate
+
+# defining the symbolic variable 'z'
+x = symbols('x')
+c0 = symbols('c0')
+c1 = symbols('c1')
+c2 = symbols('c2')
+c3 = symbols('c3')
+
+# setting up the complex equation z^2 + 1 = 0
+equation = Eq((c0*x + c1)**2 + c2*x**2 + c3, 0)
+
+# solving the equation symbolically to find complex solutions
+solutions = solve(equation, x)
+
+# printing solutions
+print(f'Solutions {solutions}')
+
+# Fill in numerical values
+alpha = ((1.0 - 2.0 * a3 * a3.conjugate())/(2.0 * a1 * a1.conjugate()))**0.5
+nc0 = a2 * a1.conjugate()
+nc1 = alpha * a1 * a3.conjugate() - a3 * a1.conjugate()
+nc2 = - a1 * a1.conjugate() * a2 * a2.conjugate()
+nc3 = 2.0 * a3 * a3.conjugate() * a1 * a1.conjugate() - a1 * a1.conjugate() / 2.0
+
+print(f'c0 = {nc0}, c1 = {nc1}, c2 = {nc2}, c3 = {nc3}')
+
+delta0 = lambdify([c0, c1, c2, c3], solutions[0], 'numpy')
+delta1 = lambdify([c0, c1, c2, c3], solutions[1], 'numpy')
+
+print(f'delta0 = {delta0(nc0, nc1, nc2, nc3)}')
+print(f'delta0 = {simplify(solutions[0].subs([(c0, nc0), (c1, nc1), (c2, nc2), (c3, nc3)]))}')
+print(f'delta1 = {delta1(nc0, nc1, nc2, nc3)}')
+print(f'delta1 = {simplify(solutions[1].subs([(c0, nc0), (c1, nc1), (c2, nc2), (c3, nc3)]))}')
+
+delta = delta1(nc0, nc1, nc2, nc3)
+beta = (delta * a2 * a1.conjugate() + nc1) / (a1 * a1.conjugate())
+
+alpha = 1.0206218666596347+0j
+beta = 1.012424035110852+0j
+
+# ORTHOGONALIZED BC
+s_a1 = symbols('a_1')
+s_a2 = symbols('a_2')
+s_a3 = symbols('a_3')
+s_alpha = symbols('alpha')
+s_beta = symbols('beta')
+s_gamma = symbols('gamma')
+s_delta = symbols('delta')
+G = Matrix([[s_alpha*s_a1, s_beta*s_a1, s_a2, s_a3, 0, 0, 0, 0], [s_a3, s_a2, s_a1, s_a1, s_a2, s_a3, 0, 0], [0, 0, s_a3, s_a2, s_a1, s_a1, s_a2, s_a3], [0, 0, 0, 0, s_a3, s_a2, s_beta*s_a1, s_alpha*s_a1]])
+barG = Matrix([[-s_gamma*conjugate(s_a1), s_delta*conjugate(s_a1), -conjugate(s_a2), conjugate(s_a3), 0, 0, 0, 0], [-conjugate(s_a3), conjugate(s_a2), -conjugate(s_a1), conjugate(s_a1), -conjugate(s_a2), conjugate(s_a3), 0, 0], [0, 0, -conjugate(s_a3), conjugate(s_a2), -conjugate(s_a1), conjugate(s_a1), -conjugate(s_a2), conjugate(s_a3)], [0, 0, 0, 0, -conjugate(s_a3), conjugate(s_a2), -s_delta*conjugate(s_a1), s_gamma*conjugate(s_a1)]])
+
+def print_corner(A, model):
+    print(f'{model}')
+    A00 = A[0, 0]
+    A01 = A[0, 1]
+    A10 = A[1, 0]
+    A11 = A[1, 1]
+    num_A00 = lambdify([s_a1, s_a2, s_a3], A00, 'numpy')
+    num_A01 = lambdify([s_a1, s_a2, s_a3], A01, 'numpy')
+    num_A10 = lambdify([s_a1, s_a2, s_a3], A10, 'numpy')
+    num_A11 = lambdify([s_a1, s_a2, s_a3], A11, 'numpy')
+    print(f'A[0, 0] {A00}')# = {num_A00(a1, a2, a3)}')
+    print(f'A[0, 1] {A01}')# = {num_A01(a1, a2, a3)}')
+    print(f'A[1, 0] {A10}')# = {num_A10(a1, a2, a3)}')
+    print(f'A[1, 1] {A11}')# = {num_A11(a1, a2, a3)}')
+    return A00, A01, A10, A11, num_A00, num_A01, num_A10, num_A11 
+
+# Same s_alpha, s_beta multiplier of G and barG - DOES NOT WORK
+#Og00, Og01, Og10, Og11, num_Og00, num_Og01, num_Og10, num_Og11 = print_corner(G * (barG.subs([(s_gamma, s_alpha), (s_delta, s_beta)])).H, 'G * barG.H')
+#Iv00, Iv01, Iv10, Iv11, num_Iv00, num_Iv01, num_Iv10, num_Iv11 = print_corner(G.H * G + (barG.subs([(s_gamma, s_alpha), (s_delta, s_beta)])).H * barG.subs([(s_gamma, s_alpha), (s_delta, s_beta)]), 'G.H * G + barG.H * barG')
+
+# s_alpha, s_beta multiplier of G and s_alpha^*, s_beta^* multiplier of barG - WORKS!!!
+Og00, Og01, Og10, Og11, num_Og00, num_Og01, num_Og10, num_Og11 = print_corner(G * (barG.subs([(s_gamma, conjugate(s_alpha)), (s_delta, conjugate(s_beta))])).H, 'G * barG.H')
+Iv00, Iv01, Iv10, Iv11, num_Iv00, num_Iv01, num_Iv10, num_Iv11 = print_corner(G.H * G + (barG.subs([(s_gamma, conjugate(s_alpha)), (s_delta, conjugate(s_beta))])).H * barG.subs([(s_gamma, conjugate(s_alpha)), (s_delta, conjugate(s_beta))]), 'G.H * G + barG.H * barG')
+
+# Constrain is to satisfy orthogonality
+equations = [Og00, Og01]
+solutions = solve(equations, s_alpha, s_beta, dict=True)
+num_s_alpha = lambdify([s_a1, s_a2, s_a3], solutions[0][s_alpha], 'numpy')
+num_s_beta = lambdify([s_a1, s_a2, s_a3], solutions[0][s_beta], 'numpy')
+print(f's_alpha {solutions[0][s_alpha]} = {num_s_alpha(a1, a2, a3)}')
+print(f's_beta {solutions[0][s_beta]} = {num_s_beta(a1, a2, a3)}')
+
+alpha = num_s_alpha(a1, a2, a3)# -0.27049867496807767-0.9841259116958961j
+beta = num_s_beta(a1, a2, a3)# 0.2377035286932745+0.9841258415511143j
+
+print(f'alpha = {alpha}')
+print(f'beta = {beta}')
+####################################################
+
 # Symmetry and unitary scaling 1/sqrt(2)
 scale = 1.0 / 2.0**0.0
 dec_lo = scale * np.array([a3, a2, a1, a1, a2, a3], dtype=complex)
-wavelets.append(['SDW2', dec_lo, [1.0, 1.0]])
+wavelets.append(['SDW2', dec_lo, [alpha, beta]])
 
 for wavelet, dec_lo, scaling in wavelets:
     N = len(dec_lo)
@@ -166,8 +262,15 @@ for wavelet, dec_lo, scaling in wavelets:
         scale_bar_mat_G00 = scale.conjugate()
         # Note the BC cut is on a reversed filter (compatible with the matrix above)
         # Apply alpha scale
-        dec_lo_BCL[3] = scale * dec_lo_BCL[3]
-        dec_hi_BCL[3] = scale.conjugate() * dec_hi_BCL[3]
+        dec_lo_BCL[3] = alpha * dec_lo_BCL[3]
+        dec_hi_BCL[3] = alpha.conjugate() * dec_hi_BCL[3]
+        #dec_lo_BCL[3] = scale * dec_lo_BCL[3]
+        #dec_hi_BCL[3] = scale.conjugate() * dec_hi_BCL[3]
+        # Apply delta scale
+        #delta = 1.0
+        dec_lo_BCL[1] = delta * dec_lo_BCL[1]
+        dec_hi_BCL[1] = delta.conjugate() * dec_hi_BCL[1]
+        # Go for beta
         prod_BCL = dec_lo_BCL * dec_hi_BCL.conjugate()
         beta = (- (prod_BCL[0] + prod_BCL[1] + prod_BCL[3]) / prod_BCL[2])**0.5
         print(f'beta scale {beta}')
@@ -176,21 +279,29 @@ for wavelet, dec_lo, scaling in wavelets:
         prod_BCL = dec_lo_BCL * dec_hi_BCL.conjugate()
         print(f'prod_BCL = dec_lo_BCL * dec_hi_BCL {prod_BCL}, sum(prod_BCL) {sum(prod_BCL)}')
         #beta = 1.0287543742494876+0j
+        #scaling = [alpha, beta, delta]
+        print(f'scaling [alpha, beta, delta] = {scaling}')
 
     # Apply the tailored scaling
-    print(f'mod {mod}, scale_mat_G00 {scale_mat_G00}, scale_bar_mat_G00 {scale_bar_mat_G00}')
-    i = 0; j = 0
-    mat_G[i, j] = scale_mat_G00 * mat_G[i, j]
-    bar_mat_G[i, j] = scale_bar_mat_G00 * bar_mat_G[i, j]
-    if (mod == 3):
-       mat_G[i, j+1] = beta * mat_G[i, j+1]
-       bar_mat_G[i, j+1] = beta.conjugate() * bar_mat_G[i, j+1]
-    i = Nrows-1; j = Ncols-1
-    mat_G[i, j] = scale_mat_G00 * mat_G[i, j]
-    bar_mat_G[i, j] = scale_bar_mat_G00 * bar_mat_G[i, j]
-    if (mod == 3):
-       mat_G[i, j-1] = beta * mat_G[i, j-1]
-       bar_mat_G[i, j-1] = beta.conjugate() * bar_mat_G[i, j-1]
+    print(f'scaling of BC coefficients (first and last row): {scaling}')
+    for j in range(len(scaling)):
+        mat_G[0, j] = scaling[j] * mat_G[0, j]
+        mat_G[Nrows-1][Ncols-1-j] = scaling[j] * mat_G[Nrows-1][Ncols-1-j] 
+        bar_mat_G[0, j] = scaling[j].conjugate() * bar_mat_G[0, j]
+        bar_mat_G[Nrows-1][Ncols-1-j] = scaling[j].conjugate() * bar_mat_G[Nrows-1][Ncols-1-j]
+#    print(f'mod {mod}, scale_mat_G00 {scale_mat_G00}, scale_bar_mat_G00 {scale_bar_mat_G00}')
+#    i = 0; j = 0
+#    mat_G[i, j] = scale_mat_G00 * mat_G[i, j]
+#    bar_mat_G[i, j] = scale_bar_mat_G00 * bar_mat_G[i, j]
+#    if (mod == 3):
+#       mat_G[i, j+1] = beta * mat_G[i, j+1]
+#       bar_mat_G[i, j+1] = beta.conjugate() * bar_mat_G[i, j+1]
+#    i = Nrows-1; j = Ncols-1
+#    mat_G[i, j] = scale_mat_G00 * mat_G[i, j]
+#    bar_mat_G[i, j] = scale_bar_mat_G00 * bar_mat_G[i, j]
+#    if (mod == 3):
+#       mat_G[i, j-1] = beta * mat_G[i, j-1]
+#       bar_mat_G[i, j-1] = beta.conjugate() * bar_mat_G[i, j-1]
 
     mat_G = np.matrix(mat_G)
     bar_mat_G = np.matrix(bar_mat_G)
@@ -202,49 +313,6 @@ for wavelet, dec_lo, scaling in wavelets:
     print(mat_G @ bar_mat_G.H)
     print('mat_G^H.mat_G + bar_mat_G^H.bar_mat_G')
     print(mat_G.H @ mat_G + bar_mat_G.H @ bar_mat_G)
-
-# Find the analytical solution for delta
-from sympy import symbols, Eq, solve, I, simplify, lambdify
-
-# defining the symbolic variable 'z'
-x = symbols('x')
-c0 = symbols('c0')
-c1 = symbols('c1')
-c2 = symbols('c2')
-c3 = symbols('c3')
-
-# setting up the complex equation z^2 + 1 = 0
-equation = Eq((c0*x + c1)**2 + c2*x**2 + c3, 0)
-
-# solving the equation symbolically to find complex solutions
-solutions = solve(equation, x)
-
-# printing solutions
-print(f'Solutions {solutions}')
-
-# Fill in numerical values
-alpha = ((1.0 - 2.0 * a3 * a3.conjugate())/(2.0 * a1 * a1.conjugate()))**0.5
-nc0 = a2 * a1.conjugate()
-nc1 = alpha * a1 * a3.conjugate() - a3 * a1.conjugate()
-nc2 = - a1 * a1.conjugate() * a2 * a2.conjugate()
-nc3 = 2.0 * a3 * a3.conjugate() * a1 * a1.conjugate() - a1 * a1.conjugate() / 2.0
-
-print(f'c0 = {nc0}, c1 = {nc1}, c2 = {nc2}, c3 = {nc3}')
-
-delta0 = lambdify([c0, c1, c2, c3], solutions[0], 'numpy')
-delta1 = lambdify([c0, c1, c2, c3], solutions[1], 'numpy')
-
-print(f'delta0 = {delta0(nc0, nc1, nc2, nc3)}')
-print(f'delta0 = {simplify(solutions[0].subs([(c0, nc0), (c1, nc1), (c2, nc2), (c3, nc3)]))}')
-print(f'delta1 = {delta1(nc0, nc1, nc2, nc3)}')
-print(f'delta1 = {simplify(solutions[1].subs([(c0, nc0), (c1, nc1), (c2, nc2), (c3, nc3)]))}')
-
-delta = delta1(nc0, nc1, nc2, nc3)
-beta = (delta * a2 * a1.conjugate() + nc1) / (a1 * a1.conjugate())
-
-print(f'alpha = {alpha}')
-print(f'beta = {beta}')
-print(f'delta = {delta}')
 
 #    # Construct 
 #    tmp = dec_lo_BCL * dec_hi_BCL
